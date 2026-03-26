5dbcb8e779b8278a274ecb6515be2a7897379452
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2026-03-27 06:43:58 +1300

    CI fixes

--- cmd/cdd-go/to_docs_json.go
+++ cmd/cdd-go/to_docs_json.go
@@ -5,124 +5,151 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/samuel/cdd-go/src/openapi"
 )
 
 type DocsJSONOutput struct {
-        Endpoints map[string]map[string]string `json:"endpoints"`
+	Language   string          `json:"language"`
+	Operations []DocsOperation `json:"operations"`
+}
+
+type DocsOperation struct {
+	Method      string   `json:"method"`
+	Path        string   `json:"path"`
+	OperationId string   `json:"operationId"`
+	Code        DocsCode `json:"code"`
+}
+
+type DocsCode struct {
+	Imports      *string `json:"imports,omitempty"`
+	WrapperStart *string `json:"wrapperStart,omitempty"`
+	WrapperEnd   *string `json:"wrapperEnd,omitempty"`
+	Snippet      string  `json:"snippet"`
 }
 
 func runToDocsJSON(args []string) error {
-        fs := flag.NewFlagSet("to_docs_json", flag.ContinueOnError)
-        fs.SetOutput(os.Stderr)
+	fs := flag.NewFlagSet("to_docs_json", flag.ContinueOnError)
+	fs.SetOutput(os.Stderr)
 
-        var in string
-        var out string
-        var noImports bool
-        var noWrapping bool
+	var in string
+	var out string
+	var noImports bool
+	var noWrapping bool
 
-        fs.StringVar(&in, "i", envOrDefault("CDD_GO_INPUT", ""), "Input file path")
-        fs.StringVar(&in, "input", envOrDefault("CDD_GO_INPUT", ""), "Input file path")
-        fs.StringVar(&out, "o", envOrDefault("CDD_GO_OUTPUT", ""), "Output file path")
-        fs.BoolVar(&noImports, "no-imports", envOrDefaultBool("CDD_GO_NO_IMPORTS", false), "Omit imports")
-        fs.BoolVar(&noWrapping, "no-wrapping", envOrDefaultBool("CDD_GO_NO_WRAPPING", false), "Omit wrapping")
+	fs.StringVar(&in, "i", envOrDefault("CDD_GO_INPUT", ""), "Input file path")
+	fs.StringVar(&in, "input", envOrDefault("CDD_GO_INPUT", ""), "Input file path")
+	fs.StringVar(&out, "o", envOrDefault("CDD_GO_OUTPUT", ""), "Output file path")
+	fs.BoolVar(&noImports, "no-imports", envOrDefaultBool("CDD_GO_NO_IMPORTS", false), "Omit imports")
+	fs.BoolVar(&noWrapping, "no-wrapping", envOrDefaultBool("CDD_GO_NO_WRAPPING", false), "Omit wrapping")
 
-        if err := fs.Parse(args); err != nil {
-                return err
-        }
-
-        if in == "" {
-                return fmt.Errorf("input file is required")
-        }
-
-        f, err := os.Open(in)
-        if err != nil {
-                return err
-        }
-        defer f.Close()
-
-        oa, err := openapi.Parse(f)
-        if err != nil {
-                return err
-        }
-
-        endpoints := make(map[string]map[string]string)
-        
-        for path, item := range oa.Paths {
-                methods := map[string]*openapi.Operation{
-                        "get":     item.Get,
-                        "put":     item.Put,
-                        "post":    item.Post,
-                        "delete":  item.Delete,
-                        "options": item.Options,
-                        "head":    item.Head,
-                        "patch":   item.Patch,
-                        "trace":   item.Trace,
-                }
-
-                pathMap := make(map[string]string)
-
-                for method, op := range methods {
-                        if op == nil {
-                                continue
-                        }
-
-                        opID := op.OperationID
-                        if opID == "" {
-                            opID = "request"
-                        }
-
-                        finalCode := ""
-                        if !noImports {
-                                finalCode += "import (\n\t\"context\"\n\t\"fmt\"\n\t\"os\"\n\t\"github.com/your/client\"\n)\n\n"
-                        }
-                        
-                        if !noWrapping {
-                                finalCode += "func main() {\n"
-                        }
-
-                        finalCode += fmt.Sprintf(`	client := client.NewAPIClient(client.NewConfiguration())
-	resp, r, err := client.DefaultApi.%s(context.Background()).Execute()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %%v
-", err)
-		os.Exit(1)
+	if err := fs.Parse(args); err != nil {
+		return err
 	}
-	fmt.Fprintf(os.Stdout, "Response: %%v
-", resp)`, opID)
 
-                        if !noWrapping {
-                                finalCode += "\n}"
-                        }
-                        
-                        pathMap[method] = finalCode
-                }
-                
-                if len(pathMap) > 0 {
-                    endpoints[path] = pathMap
-                }
-        }
+	if in == "" {
+		return fmt.Errorf("input file is required")
+	}
 
-        result := DocsJSONOutput{
-                Endpoints: endpoints,
-        }
+	f, err := os.Open(in)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
 
-        outTarget := os.Stdout
-        if out != "" {
-                fileTarget, err := os.Create(out)
-                if err != nil {
-                        return err
-                }
-                defer fileTarget.Close()
-                outTarget = fileTarget
-        }
+	oa, err := openapi.Parse(f)
+	if err != nil {
+		return err
+	}
 
-        encoder := json.NewEncoder(outTarget)
-        encoder.SetIndent("", "  ")
-        if err := encoder.Encode(result); err != nil {
-                return err
-        }
+	var operations []DocsOperation
 
-        return nil
+	for path, item := range oa.Paths {
+		methods := map[string]*openapi.Operation{
+			"get":     item.Get,
+			"put":     item.Put,
+			"post":    item.Post,
+			"delete":  item.Delete,
+			"options": item.Options,
+			"head":    item.Head,
+			"patch":   item.Patch,
+			"trace":   item.Trace,
+		}
+
+		for method, op := range methods {
+			if op == nil {
+				continue
+			}
+
+			opID := op.OperationID
+			if opID == "" {
+				opID = "request"
+			}
+
+			var imports *string
+			if !noImports {
+				i := "import (\n\t\"context\"\n\t\"fmt\"\n\t\"os\"\n\t\"github.com/your/client\"\n)"
+				imports = &i
+			}
+
+			var wrapperStart *string
+			var wrapperEnd *string
+			if !noWrapping {
+				ws := "func main() {"
+				wrapperStart = &ws
+				we := "}"
+				wrapperEnd = &we
+			}
+
+			snippet := fmt.Sprintf(`client := client.NewAPIClient(client.NewConfiguration())
+resp, r, err := client.DefaultApi.%s(context.Background()).Execute()
+if err != nil {
+	fmt.Fprintf(os.Stderr, "Error: %%v\n", err)
+	os.Exit(1)
+}
+fmt.Fprintf(os.Stdout, "Response: %%v\n", resp)`, opID)
+
+			operations = append(operations, DocsOperation{
+				Method:      strings.ToUpper(method),
+				Path:        path,
+				OperationId: opID,
+				Code: DocsCode{
+					Imports:      imports,
+					WrapperStart: wrapperStart,
+					WrapperEnd:   wrapperEnd,
+					Snippet:      snippet,
+				},
+			})
+		}
+	}
+
+	if operations == nil {
+		operations = []DocsOperation{}
+	}
+
+	result := []DocsJSONOutput{
+		{
+			Language:   "go",
+			Operations: operations,
+		},
+	}
+
+	outTarget := os.Stdout
+	if out != "" {
+		fileTarget, err := os.Create(out)
+		if err != nil {
+			return err
+		}
+		defer fileTarget.Close()
+		outTarget = fileTarget
+	}
+
+	encoder := json.NewEncoder(outTarget)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(result); err != nil {
+		return err
+	}
+
+	return nil
 }
